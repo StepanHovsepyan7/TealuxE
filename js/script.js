57fcd6f7b4bd3f9fd2297b7e97d41058aaf4812e
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-02-05 15:07:15 +0400

    finished the responsive part of the header and the top part of the main

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let barsA = document.getElementsByClassName('barsA')[0];
+let menuCloseBtn = document.getElementsByClassName('menuCloseBtn')[0];
+let menuList = document.getElementsByClassName('menuList')[0];
+let menuCloseMobile = document.getElementsByClassName('menu-Close')[0];
+
+
+barsA.addEventListener('click', () => {
+    menuList.classList.toggle('menuListOpen');
+    menuCloseMobile.classList.toggle('menuOpen');
+
+})
+
+menuCloseBtn.addEventListener('click', () =>{
+    menuList.classList.remove('menuListOpen')
+    menuCloseMobile.classList.remove('menuOpen');
+})
+
